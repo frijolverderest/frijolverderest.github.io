6df5d519051ddac2f8123c3c6f014a3fc4b860a2
Jamesss73 <jamesmunozs@unicauca.edu.co>
2024-01-04 01:40:26 -0500

    i worked on product card

--- scripts/actions.js
+++ scripts/actions.js
@@ -4,3 +4,28 @@ window.addEventListener('scroll', function() {
     const scrolled = window.scrollY;
     parallax.style.transform = 'translateY(' + (scrolled * 0.05) + 'px)';
 });
+
+function showDetails(plateId) {
+    const modal = document.getElementById('modal');
+    const modalImg = document.getElementById('modal-img');
+    const modalDetails = document.getElementById('modal-details');
+
+    modalImg.src = `../files/imgOurMenu/${plateId}.jpeg`;
+
+    // Texto de prueba para la descripción larga del plato
+    modalDetails.innerHTML = "<h2>Nombre del plato</h2><p>Esta es una descripción larga de ejemplo para el plato. Incluye detalles sobre los ingredientes y la preparación del plato. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>";
+
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
