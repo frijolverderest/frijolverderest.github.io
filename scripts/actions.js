fafb73f705bc1a56be74c232d354540bf9766dc1
Jamesss73 <jamesmunozs@unicauca.edu.co>
2024-01-15 00:48:53 -0500

    avance en menu y en carusel de index

--- scripts/actions.js
+++ scripts/actions.js
@@ -5,6 +5,38 @@ window.addEventListener('scroll', function() {
     parallax.style.transform = 'translateY(' + (scrolled * 0.05) + 'px)';
 });
 
+ 
+window.addEventListener('scroll', function() {
+    const plateps = document.querySelectorAll('.platep');
+    const scrolled = window.scrollY;
+
+    plateps.forEach((platep) => {
+        // Calcula la posición relativa y aplica transformación translateY
+        //platep.style.transform = 'translateY(' + (scrolled * 0.05) + 'px)';
+
+        // Agrega la clase de animación cuando el elemento está visible en la pantalla
+        if (isElementInViewport(platep)) {
+            platep.classList.add('zoomIn');
+        } else {
+            // Puedes quitar la clase aquí si prefieres que la animación se reinicie cada vez que el elemento sale de la vista
+            platep.classList.remove('zoomIn');
+        }
+    });
+});
+
+// Función para verificar si un elemento está en la vista
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+
 function showDetails(plateId) {
     const modal = document.getElementById('modal');
     const modalImg = document.getElementById('modal-img');
