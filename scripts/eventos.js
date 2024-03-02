3e1d28e7bf47cdb439372d094555d584c08b990f
Jamesss73 <jamesmunozs@unicauca.edu.co>
2024-03-01 19:48:10 -0500

    add modal en eventos2

---
+++ scripts/eventos.js
@@ -0,0 +1,17 @@
+    // Función para abrir el modal
+    function openModal() {
+        document.getElementById('myModal').style.display = 'block';
+    }
+
+    // Función para cerrar el modal
+    function closeModal() {
+        document.getElementById('myModal').style.display = 'none';
+    }
+
+    // Cerrar el modal si se hace clic fuera de él
+    window.onclick = function(event) {
+        var modal = document.getElementById('myModal');
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
