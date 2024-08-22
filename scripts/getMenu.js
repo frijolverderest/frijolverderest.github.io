f15fd4bd209935f43cd064b2b50fba7036a79892
James Munoz Soscue <jamesmunozs@unicauca.edu.co>
2024-08-22 15:27:01 -0500

    ajustes en modal de menu

--- scripts/getMenu.js
+++ scripts/getMenu.js
@@ -1,5 +1,5 @@
 // Ruta del archivo JSON
-const jsonFilePath = '../files/menu.json';
+const jsonFilePath = "../files/menu.json";
 
 // Variable global para almacenar los datos de los platos
 let platos;
@@ -11,51 +11,48 @@ async function cargarJSON() {
     platos = await response.json();
     // Muestra los platos
     mostrarPlatos(platos.menu);
-
   } catch (error) {
-    console.error('Error al cargar el archivo JSON:', error);
+    console.error("Error al cargar el archivo JSON:", error);
   }
 }
 
-
-
 // Función para mostrar los platos en la sección correspondiente
 function mostrarPlatos(menu) {
-  const contenedorEntradas = document.getElementById('menuEntradas');
+  const contenedorEntradas = document.getElementById("menuEntradas");
   if (contenedorEntradas) {
     cargarCategoria(contenedorEntradas, menu.entradas);
   }
 
-  const contenedorFrijol = document.getElementById('menuConFrijol');
+  const contenedorFrijol = document.getElementById("menuConFrijol");
   if (contenedorFrijol) {
     cargarCategoria(contenedorFrijol, menu.platosConFrijol);
   }
 
-  const contenedorRio = document.getElementById('menuDelRio');
+  const contenedorRio = document.getElementById("menuDelRio");
   if (contenedorRio) {
     cargarCategoria(contenedorRio, menu.delRio);
   }
 
-  const contenedorCarnes = document.getElementById('menuCarnes');
+  const contenedorCarnes = document.getElementById("menuCarnes");
   if (contenedorCarnes) {
     cargarCategoria(contenedorCarnes, menu.carnes);
   }
 
-  const contenedorBebidas = document.getElementById('menuBebidas');
+  const contenedorBebidas = document.getElementById("menuBebidas");
   if (contenedorBebidas) {
     cargarCategoria(contenedorBebidas, menu.bebidas);
   }
-  
-  const contenedorPostres = document.getElementById('menuPostres');
+
+  const contenedorPostres = document.getElementById("menuPostres");
   if (contenedorPostres) {
     cargarCategoria(contenedorPostres, menu.postres);
   }
 
-  const contenedorDesayunos = document.getElementById('menuDesayunos');
+  const contenedorDesayunos = document.getElementById("menuDesayunos");
   if (contenedorDesayunos) {
     cargarCategoria(contenedorDesayunos, menu.desayunos);
   }
-  const contenedorPlatosjr= document.getElementById('menuPlatosjr');
+  const contenedorPlatosjr = document.getElementById("menuPlatosjr");
   if (contenedorPlatosjr) {
     cargarCategoria(contenedorPlatosjr, menu.platosJunior);
   }
@@ -63,44 +60,37 @@ function mostrarPlatos(menu) {
 
 function cargarCategoria(contenedor, categoria) {
   categoria.forEach((plato) => {
-    const platoDiv = document.createElement('div');
-    platoDiv.classList.add('plato-card'); // Agrega la clase 'plato-card'
-    //platoDiv.classList.add('animated'); // Agrega la clase 'animated' para iniciar la animación
-    
-  platoDiv.onclick = () => showDetails(plato);
-   // platoDiv.style.opacity = '0'; // Inicia invisible
-    //platoDiv.style.transition = 'transform: scale(0.5)';
-   // platoDiv.style.animation = 'zoomIn 0.5s ease-out';
-   //platoDiv.classList.add('zoomIn');
+    const platoDiv = document.createElement("div");
+    platoDiv.classList.add("plato-card");
 
-    //platoDiv.addEventListener('mouseover', () => showDetails(`p${entrada.id}`));
-    //platoDiv.addEventListener('mouseout', () => closeModal());
-    
-    const platoDetallesDiv = document.createElement('div');
-    platoDetallesDiv.classList.add('plato-detalles');
+    platoDiv.onclick = () => showDetails(plato);
 
-    const platoH2 = document.createElement('h2');
-    platoH2.classList.add('plato-nombre');
+    const platoDetallesDiv = document.createElement("div");
+    platoDetallesDiv.classList.add("plato-detalles");
+
+    const platoH2 = document.createElement("h2");
+    platoH2.classList.add("plato-nombre");
     platoH2.textContent = plato.nombre.toUpperCase();
 
-    const descripcionP = document.createElement('p');
-    descripcionP.classList.add('plato-descripcion');
+    const descripcionP = document.createElement("p");
+    descripcionP.classList.add("plato-descripcion");
     descripcionP.textContent = plato.descripcionCorta;
 
-    const platoHr = document.createElement('hr');
-    platoHr.classList.add('plato-hr');
+    const platoHr = document.createElement("hr");
+    platoHr.classList.add("plato-hr");
 
-    const precioP = document.createElement('p');
-    precioP.classList.add('plato-precio');
-    precioP.textContent = "$" + plato.precio.toFixed(3);
+    const precioP = document.createElement("p");
+    precioP.classList.add("plato-precio");
+    precioP.textContent = "$" + plato.precio.toLocaleString('de-DE');
 
-//******* */
+    // precioP.textContent = "$" + plato.precio.toFixed(3);
 
-    const img = document.createElement('img');
-    img.classList.add('plato-img');
+    //******* */
+
+    const img = document.createElement("img");
+    img.classList.add("plato-img");
     img.src = `files/imgmenu/${plato.fotografias[0]}`;
     img.alt = `Plato ${plato.id} - Foto 1`;
-
 
     platoDetallesDiv.appendChild(platoH2);
     platoDetallesDiv.appendChild(descripcionP);
@@ -111,66 +101,135 @@ function cargarCategoria(contenedor, categoria) {
     platoDiv.appendChild(img);
 
     contenedor.appendChild(platoDiv);
-
- 
-    
-
   });
 }
 
-
 function showDetails(_plato) {
-  document.getElementById('myModal').style.display = 'block';
+  document.getElementById("myModal").style.display = "block";
 
-  // Para el primer item
-  document.querySelector('.carousel-inner .item:nth-child(1) .modal-img').src = `files/imgmenu/${_plato.fotografias[0]}`;//'files/imgeventos/graduacion2.jpg';
-  document.querySelector('.carousel-inner .item:nth-child(1) h2').innerText =  _plato.nombre; //'Graduaciones 1';
-  document.querySelector('.carousel-inner .item:nth-child(1) p').innerText = _plato.descripcionCorta; //'Disfruta de una decoración encantadora para tu graduación en Frijol Verde';
+  const carouselInner = document.querySelector(".carousel-inner");
+  carouselInner.innerHTML = ''; // Limpiar el contenido existente
 
-  // Para el segundo item
-  document.querySelector('.carousel-inner .item:nth-child(2) .modal-img').src = `files/imgmenu/${_plato.fotografias[0]}`;//'files/imgeventos/graduacion.jpg';
-  document.querySelector('.carousel-inner .item:nth-child(2) h2').innerText =  _plato.nombre; //'Graduaciones 2';
-  document.querySelector('.carousel-inner .item:nth-child(2) p').innerText = _plato.descripcionCorta;//'Disfruta de una decoración encantadora para tu graduación en Frijol Verde';
+  const indicatorsContainer = document.querySelector(".carousel-indicators");
+  indicatorsContainer.innerHTML = ''; // Limpiar indicadores existentes
 
-  // Para el tercer item
-  document.querySelector('.carousel-inner .item:nth-child(3) .modal-video').src =`files/imgmenu/${_plato.fotografias[0]}`;//'files/imgeventos/graduacionesv.mp4';
-  document.querySelector('.carousel-inner .item:nth-child(3) h2').innerText =   _plato.nombre; //'Graduaciones 3';
-  document.querySelector('.carousel-inner .item:nth-child(3) p').innerText = _plato.descripcionCorta;//'Disfruta de una decoración encantadora para tu graduación en Frijol Verde';
+  _plato.fotografias.forEach((foto, index) => {
+      // Crear el contenedor del item
+      const itemDiv = document.createElement("div");
+      itemDiv.classList.add("item");
+      if (index === 0) itemDiv.classList.add("active");
+
+      // Crear y añadir la imagen
+      const img = document.createElement("img");
+      img.classList.add("modal-img");
+      img.src = `files/imgmenu/${foto}`;
+      itemDiv.appendChild(img);
+
+      // Crear la descripción
+      const descriptionDiv = document.createElement("div");
+      descriptionDiv.classList.add("evento-description");
+
+      const h2 = document.createElement("h2");
+      h2.innerText = _plato.nombre;
+
+      const p = document.createElement("p");
+      p.innerText = _plato.descripcionCorta;
+
+      descriptionDiv.appendChild(h2);
+      descriptionDiv.appendChild(p);
+
+      itemDiv.appendChild(descriptionDiv);
+
+      // Añadir el item al carrusel
+      carouselInner.appendChild(itemDiv);
+
+      // Crear el indicador correspondiente
+      const indicator = document.createElement("li");
+      indicator.setAttribute("data-target", "#myModal");
+      indicator.setAttribute("data-slide-to", index);
+      if (index === 0) indicator.classList.add("active");
+
+      indicatorsContainer.appendChild(indicator);
+  });
+
+  _plato.videos.forEach((video, index) => {
+      const videoIndex = _plato.fotografias.length + index; // Para que los videos tengan su propio índice después de las fotos
+
+      // Crear el contenedor del item
+      const itemDiv = document.createElement("div");
+      itemDiv.classList.add("item");
+      if (videoIndex === 0) itemDiv.classList.add("active");
+
+      // Crear y añadir el video
+      const videoElement = document.createElement("video");
+      videoElement.classList.add("modal-img", "modal-video");
+      videoElement.setAttribute("loop", "");
+      videoElement.setAttribute("controls", "");
+
+      const source = document.createElement("source");
+      source.src = `files/imgmenu/${video}`;
+      source.type = "video/mp4";
+      videoElement.appendChild(source);
+
+      itemDiv.appendChild(videoElement);
+
+      // Crear la descripción
+      const descriptionDiv = document.createElement("div");
+      descriptionDiv.classList.add("evento-description");
+
+      const h2 = document.createElement("h2");
+      h2.innerText = _plato.nombre;
+
+      const p = document.createElement("p");
+      p.innerText = _plato.descripcionCorta;
+
+      descriptionDiv.appendChild(h2);
+      descriptionDiv.appendChild(p);
+
+      itemDiv.appendChild(descriptionDiv);
+
+      // Añadir el item al carrusel
+      carouselInner.appendChild(itemDiv);
+
+      // Crear el indicador correspondiente
+      const indicator = document.createElement("li");
+      indicator.setAttribute("data-target", "#myModal");
+      indicator.setAttribute("data-slide-to", videoIndex);
+      if (videoIndex === 0) indicator.classList.add("active");
+
+      indicatorsContainer.appendChild(indicator);
+  });
 }
 
 $(document).ready(function () {
-  $('.carousel').on('touchstart', function (event) {
-      const xClick = event.originalEvent.touches[0].pageX;
-      $(this).one('touchmove', function (event) {
-          const xMove = event.originalEvent.touches[0].pageX;
-          const sensitivityInPx = 5; // Sensibilidad del deslizamiento (ajústala según tu preferencia)
-          if (Math.floor(xClick - xMove) > sensitivityInPx) {
-              $(this).carousel('next');
-          }
-          else if (Math.floor(xClick - xMove) < -sensitivityInPx) {
-              $(this).carousel('prev');
-          }
-      });
-      $(this).on('touchend', function () {
-          $(this).off('touchmove');
-      });
+  $(".carousel").on("touchstart", function (event) {
+    const xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function (event) {
+      const xMove = event.originalEvent.touches[0].pageX;
+      const sensitivityInPx = 5; // Sensibilidad del deslizamiento (ajústala según tu preferencia)
+      if (Math.floor(xClick - xMove) > sensitivityInPx) {
+        $(this).carousel("next");
+      } else if (Math.floor(xClick - xMove) < -sensitivityInPx) {
+        $(this).carousel("prev");
+      }
+    });
+    $(this).on("touchend", function () {
+      $(this).off("touchmove");
+    });
   });
 });
 
-
-window.addEventListener('scroll', function () {
-  var video = document.getElementById('videoGraduaciones');
+window.addEventListener("scroll", function () {
+  var video = document.getElementById("videoGraduaciones");
   var position = video.getBoundingClientRect().top;
   var screenHeight = window.innerHeight;
 
   if (position < screenHeight && position > 0) {
-      video.play();
+    video.play();
   } else {
-      video.pause();
+    video.pause();
   }
 });
-
-
 
 /*
 
@@ -242,48 +301,55 @@ function showDetails(platoId) {
 
 // Función para cerrar el modal
 function closeModal() {
-  const modal = document.getElementById('myModal');
-  modal.style.display = 'none';
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
 
 // Cerrar el modal si se hace clic fuera de él
 window.onclick = function (event) {
-  var modal = document.getElementById('myModal');
-  if (event.target == modal) {
-      var video = document.getElementById('videoGraduaciones');
+  var modal = document.getElementById("myModal");
+  var modalContent = document.querySelector(".modal-content");
+  
+  // Verifica si el clic ocurrió fuera del contenido del modal
+  if (event.target == modal && !modalContent.contains(event.target)) {
+    var video = document.getElementById("videoGraduaciones");
+    if (video) {
       video.pause();
-      modal.style.display = 'none';
+    }
+    modal.style.display = "none";
   }
-}
+};
+
 
 // Llama a la función para cargar el JSON al cargar la página
 window.onload = cargarJSON;
 
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              entry.target.style.opacity = 1;
-              entry.target.style.transform = 'scale(1)';
-          } else {
-              entry.target.style.opacity = 0;
-              entry.target.style.transform = 'scale(0.5)';
-          }
+document.addEventListener("DOMContentLoaded", (event) => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.style.opacity = 1;
+          entry.target.style.transform = "scale(1)";
+        } else {
+          entry.target.style.opacity = 0;
+          entry.target.style.transform = "scale(0.5)";
+        }
       });
-  }, { threshold: 0.1 }); // Ajusta el umbral según necesidad
+    },
+    { threshold: 0.1 }
+  ); // Ajusta el umbral según necesidad
 
   // Observa todos los elementos .plato-card
-  document.querySelectorAll('.plato-card').forEach(card => {
-      observer.observe(card);
+  document.querySelectorAll(".plato-card").forEach((card) => {
+    observer.observe(card);
   });
 
   // Función para observar elementos .plato-card agregados dinámicamente
   function observarElementosDinamicos() {
-      document.querySelectorAll('.plato-card').forEach(card => {
-          observer.observe(card);
-      });
+    document.querySelectorAll(".plato-card").forEach((card) => {
+      observer.observe(card);
+    });
   }
 
   // Llama a la función para observar los elementos dinámicos
