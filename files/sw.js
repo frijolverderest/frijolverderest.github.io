4e27d8da23d297573f4277ae43c2c0fd4b12ecf2
James Munoz Soscue <jamesmunozs@unicauca.edu.co>
2024-09-19 10:07:51 -0500

    added pwa compatibility

---
+++ files/sw.js
@@ -0,0 +1,47 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('frijol-verde-cache').then((cache) => {
+        return cache.addAll([
+          './',
+          './index.html',
+          './about.html',
+          './contacto.html',
+          './desayunos.html',
+          './eventos.html',
+          './eventos2.html',
+          './indexdes.html',
+          './nuestrmenu.html',
+          './nuestrmenu.css',
+          './nuestrmenudesarrollo.html',
+          './nuestrmenudesarrollocopy.html',
+          './platosjr.html',
+          './CNAME',
+          './scripts/actions.js',
+          './scripts/eventos.js',
+          './scripts/getEventos.js',
+          './scripts/getMenu.js',
+          './scripts/index.js',
+          './scripts/script1.js',
+          './styles/contacto.css',
+          './styles/eventos.css',
+          './styles/eventos2.css',
+          './styles/footer.css',
+          './styles/index.css',
+          './styles/navbar.css',
+          './styles/nuestrmenu.css',
+          './styles/style.css',
+          './manifest.json'
+          // Si agregas más archivos en el futuro, inclúyelos aquí
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
